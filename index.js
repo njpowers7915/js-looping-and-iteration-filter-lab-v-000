d5ba5f2f920b36fad4326edca6ef0d0feb513736
Nicholas Powers <njpowers7915@gmail.com>
2018-07-24 17:17:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
 function findMatching(drivers, string) {
-  drivers.filter(function(x) {return x == string;});
+  drivers.toLowerCase().filter(function(x) {return x == string.toLowerCase();});
 }
 
 function fuzzyMatch() {
