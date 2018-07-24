e6731727a78781e25f641eb86330400c492e5d9f
Nicholas Powers <njpowers7915@gmail.com>
2018-07-24 17:21:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function fuzzyMatch(drivers, string) {
 }
 
 function matchName(drivers, string) {
-  return drivers.filter(function(x) {return x.name === string};);
+  return drivers.filter(function(x) {return x.name === string;});
 }
